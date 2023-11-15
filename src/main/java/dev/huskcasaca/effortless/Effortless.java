a4ceceaa904d3ad5045a7b1a7fe7d5e207cd8996
Johannes Loehnert <loehnert.kde@gmx.net>
2023-11-15 21:18:07 +0100

    move serverside handler to Effortless.java and remove EffortlessServer .. turns out, the entrypoint is for Dedicated server only fixes previously broken release

--- src/main/java/dev/huskcasaca/effortless/Effortless.java
+++ src/main/java/dev/huskcasaca/effortless/Effortless.java
@@ -1,5 +1,6 @@
 package dev.huskcasaca.effortless;
 
+import dev.huskcasaca.effortless.building.BuildActionHandler;
 import dev.huskcasaca.effortless.buildmodifier.BuildModifierHandler;
 import dev.huskcasaca.effortless.entity.player.ModeSettings;
 import dev.huskcasaca.effortless.building.ReachHelper;
@@ -9,7 +10,10 @@ import dev.huskcasaca.effortless.buildmode.BuildModeHelper;
 import dev.huskcasaca.effortless.buildmodifier.BuildModifierHelper;
 import dev.huskcasaca.effortless.buildmodifier.UndoRedo;
 import dev.huskcasaca.effortless.entity.player.ModifierSettings;
+import dev.huskcasaca.effortless.network.protocol.player.*;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
@@ -88,5 +92,52 @@ public class Effortless implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        log("Effortless.onInitialize was called.");
+        // Register serverside network packet handlers
+        ServerPlayNetworking.registerGlobalReceiver(
+                ServerboundPlayerBreakBlockPacket.TYPE, Effortless::handlePacket
+        );
+        ServerPlayNetworking.registerGlobalReceiver(
+                ServerboundPlayerBuildActionPacket.TYPE, Effortless::handlePacket
+        );
+        ServerPlayNetworking.registerGlobalReceiver(
+                ServerboundPlayerPlaceBlockPacket.TYPE, Effortless::handlePacket
+        );
+        ServerPlayNetworking.registerGlobalReceiver(
+                ServerboundPlayerSetBuildModePacket.TYPE, Effortless::handlePacket
+        );
+        ServerPlayNetworking.registerGlobalReceiver(
+                ServerboundPlayerSetBuildModifierPacket.TYPE, Effortless::handlePacket
+        );
+        ServerPlayNetworking.registerGlobalReceiver(
+                ServerboundPlayerSetBuildReachPacket.TYPE, Effortless::handlePacket
+        );
     }
+    public static void handlePacket(ServerboundPlayerBreakBlockPacket packet, ServerPlayer player, PacketSender sender) {
+        Effortless.log("handlePacket: ServerboundPlayerBreakBlockPacket");
+        BuildModeHandler.onBlockBrokenPacketReceived(player, packet);
+    }
+
+    public static void handlePacket(ServerboundPlayerBuildActionPacket packet, ServerPlayer player, PacketSender sender) {
+        BuildActionHandler.performAction(player, packet.action());
+    }
+
+    public static void handlePacket(ServerboundPlayerPlaceBlockPacket packet, ServerPlayer player, PacketSender sender) {
+        BuildModeHandler.onBlockPlacedPacketReceived(player, packet);
+    }
+    public static void handlePacket(ServerboundPlayerSetBuildModePacket packet, ServerPlayer player, PacketSender sender) {
+        BuildModeHelper.setModeSettings(player, BuildModeHelper.sanitize(packet.modeSettings(), player));
+        BuildModeHandler.initializeMode(player);
+    }
+
+
+    public static void handlePacket(ServerboundPlayerSetBuildModifierPacket packet, ServerPlayer player, PacketSender sender) {
+        BuildModifierHelper.setModifierSettings(player, BuildModifierHelper.sanitize(packet.modifierSettings(), player));
+    }
+
+    public static void handlePacket(ServerboundPlayerSetBuildReachPacket packet, ServerPlayer player, PacketSender sender) {
+        ReachHelper.setReachSettings(player, ReachHelper.sanitize(packet.reachSettings(), player));
+        BuildModeHandler.initializeMode(player);
+    }
+
 }

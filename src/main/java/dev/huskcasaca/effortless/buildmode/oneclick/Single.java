f18a3842f23841566d90a4cd6df47aa5fd631f8d
Johannes Loehnert <loehnert.kde@gmx.net>
2023-11-29 19:22:06 +0100

    Feature: in Single mode, blocks can be placed in air by looking straight down.
    Thus, starting construction in thin air becomes possible.

--- src/main/java/dev/huskcasaca/effortless/buildmode/oneclick/Single.java
+++ src/main/java/dev/huskcasaca/effortless/buildmode/oneclick/Single.java
@@ -4,6 +4,7 @@ import dev.huskcasaca.effortless.buildmode.OneClickBuildable;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.phys.Vec3;
 
 import java.util.ArrayList;
@@ -26,6 +27,12 @@ public class Single extends OneClickBuildable {
 
     @Override
     public List<BlockPos> findCoordinates(Player player, BlockPos blockPos, boolean skipRaytrace) {
+        var lookVec = player.getLookAngle();
+        // If looking straight down, unconditionally return block beneath player's feet.
+        // Thus, construction can be started in thin air.
+        if (Math.abs(lookVec.x) < 0.0001 && Math.abs(lookVec.z) < 0.0001 && lookVec.y < -0.9999) {
+            blockPos = player.blockPosition().below();
+        }
         if (blockPos == null) return Collections.emptyList();
         return getFinalBlocks(player, blockPos.getX(), blockPos.getY(), blockPos.getZ());
     }
